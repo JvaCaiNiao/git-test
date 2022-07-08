6fd736ad1dd02eba6c0b07a8e1b5b43263e51761
hzb <hzb@qq.com>
2022-07-08 20:41:55 +0800

    second commit

--- src/main/java/com/hzbedu/Git/GitTest.java
+++ src/main/java/com/hzbedu/Git/GitTest.java
@@ -6,5 +6,6 @@ package com.hzbedu.Git;/*
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git!");
+        System.out.println("hello Git2!");
     }
 }
